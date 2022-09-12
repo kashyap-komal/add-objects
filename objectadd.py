09f38340b153063b4ec40445b272221da6a1b288
kashyap-komal <65062122+kashyap-komal@users.noreply.github.com>
2022-09-12 07:52:22 +0530

    Create objectadd.py

---
+++ objectadd.py
@@ -0,0 +1,23 @@
+import boto3
+import os
+
+
+def upload_files(path):
+    session = boto3.Session(
+        aws_access_key_id='AKIAXPJK5QCBVLX2SNOQ',
+        aws_secret_access_key='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',
+        region_name='ap-south-1'
+    )
+    s3 = session.resource('s3')
+    bucket = s3.Bucket('myboto100bucket')
+
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            full_path = os.path.join(subdir, file)
+            with open(full_path, 'rb') as data:
+                bucket.put_object(Key=full_path[len(path) + 1:], Body=data)
+
+
+if __name__ == "__main__":
+    upload_files(r"C:\Users\komal.kumari12\Desktop\object")
+    #upload_files('C:\Users\komal.kumari12\Desktop\object')
